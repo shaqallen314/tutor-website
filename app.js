ed185f568fd80d15cd26b3414120e1863f8b951e
shaqallen314 <a93386@gmail.com>
2026-05-07 21:16:12 +0800

    Student Info Update V1

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const ADMIN_EMAIL = "a93386@gmail.com";
 const STUDENT_ACCOUNTS = {
     "紫軒": "a9386@gmail.com", 
     "昵貽": "a9386@gmail.com",   
-    "芳銘": "@gmail.com" 
+    "芳銘": "aliyaliao1103@gmail.com" 
 };
 
 // ==========================================
